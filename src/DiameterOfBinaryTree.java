2b4eb04860879c21fd4f81df7e0ebc6fd32a521d
Naman Ajmera <cool.naman.ajmera@gmail.com>
2025-02-11 21:23:11 +0530

    Added: Diameter of BT.

--- src/DiameterOfBinaryTree.java
+++ src/DiameterOfBinaryTree.java
@@ -1,3 +1,26 @@
+import commonFunctions.BinaryTreeFunctions;
+import commonFunctions.BinaryTreeNode;
+
 public class DiameterOfBinaryTree {
-         
+    public static void main(String[] args) {
+        BinaryTreeNode<Integer> root = BinaryTreeFunctions.createBinaryTree();
+        int result = diameterOfBinaryTree(root);
+        System.out.println(result);
+    }
+
+    static int ans = 0;
+
+    private static int diameterOfBinaryTree(BinaryTreeNode<Integer> root) {
+        heightWithDia(root);
+        return ans;
+    }
+
+    private static int heightWithDia(BinaryTreeNode<Integer> root) {
+        if (root == null)
+            return 0;
+        int lh = heightWithDia(root.left);
+        int rh = heightWithDia(root.right);
+        ans = Math.max(ans, lh + rh);
+        return 1 + Math.max(lh, rh);
+    }
 }
